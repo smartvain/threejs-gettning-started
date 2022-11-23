d2e0bf72d3c28eae57070ba83e0ca62ad372f669
smartvain <zlc.blue15210@gmail.com>
2022-11-23 18:59:05 +0900

    add: four targets

--- src/BlasterScene.ts
+++ src/BlasterScene.ts
@@ -1,17 +1,37 @@
 import * as THREE from 'three'
 
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader'
+
 export default class BlasterScene extends THREE.Scene
 {
-  initialize()
+  private readonly mtlLoader = new MTLLoader()
+  private readonly objLoader = new OBJLoader()
+  
+  async initialize()
   {
-    const geometory = new THREE.BoxGeometry()
-    const material = new THREE.MeshPhongMaterial({ color: 0xFFAD00})
+    // load a shared MTL (Material Template Library) for the targets
+    const targetMtl = await this.mtlLoader.loadAsync('assets/targetA.mtl')
+    targetMtl.preload()
 
-    const cube = new THREE.Mesh(geometory, material)
-    cube.position.z = -5
-    cube.position.y = -1
-    
-    this.add(cube)
+    // create the 4 targets
+    const t1 = await this.createTarget(targetMtl)
+    t1.position.x = -1
+    t1.position.y = -3
+
+    const t2 = await this.createTarget(targetMtl)
+    t2.position.x = 1
+    t2.position.y = -3
+
+    const t3 = await this.createTarget(targetMtl)
+    t3.position.x = 2
+    t3.position.y = -3
+
+    const t4 = await this.createTarget(targetMtl)
+    t4.position.x = -2
+    t4.position.y = -3
+
+    this.add(t1, t2, t3, t4)
 
     const light = new THREE.DirectionalLight(0xFFFFFF, 1)
     light.position.set(0, 4, 2)
