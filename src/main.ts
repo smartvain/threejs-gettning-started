9226cbd84133a00c72cb9d14632b9b21e61c3e7f
smartvain <zlc.blue15210@gmail.com>
2022-11-23 18:00:04 +0900

    add: renderer, scene, camera to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,22 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import * as THREE from 'three'
+import BlasterScene from './BlasterScene'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const width = window.innerWidth
+const height = window.innerHeight
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.getElementById('app') as HTMLCanvasElement
+})
+renderer.setSize(width, height)
+
+const mainCamera = new THREE.PerspectiveCamera(
+  60,
+  width / height,
+  0.1,
+  100,
+)
+
+const scene = new BlasterScene()
+scene.initialize()
+
+renderer.render(scene, mainCamera)
